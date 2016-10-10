c1ce3fd844abad46c2a404e52aafb2dbb9cb4fba
jose0luis41 <jose0luis41@hotmail.com>
2016-10-10 02:02:37 -0500

    Create Main.java

---
+++ Semaforo/Main.java
@@ -0,0 +1,47 @@
+package bodeguita;
+
+
+
+
+public class Main {
+
+	private Bodega bodega;
+	private Recolector recolector;
+	private Deposito depositador;
+	
+	public Main(){
+		bodega = new Bodega(0, 0);
+		recolector = new Recolector(this);
+		depositador = new Deposito(this);
+	}
+	
+	
+	
+	public void proceso(){
+		recolector.start();
+		depositador.start();
+	}
+	
+	
+	public void depositarArticulo(double articulo){
+		try {
+			bodega.colocarArticulo(articulo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void armarPaquete(){
+		try {
+			bodega.armarPaquete();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args){
+		new Main().proceso();
+	}
+	
+}
